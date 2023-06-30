d723695459da0e402f45f3099c9129fcf7aa254c
Adil Shad <115205289+adilshads@users.noreply.github.com>
2023-06-30 16:58:44 -0400

    Add validation to ensure user inputs integer

--- script.js
+++ script.js
@@ -11,6 +11,13 @@ function addBookToLibrary() {
   const author = prompt("Enter the author's name:");
   const pages = prompt("Enter the number of pages:");
 
+  // Validate and convert pages to an integer
+  while (!Number.isInteger(Number(pages))) {
+    pages = prompt("Invalid input. Please enter the number of pages as an integer:");
+  }
+  pages = parseInt(pages);
+
+  // Add new book to page from information recieved from user
   const book = new Book(title, author, pages);
   myLibrary.push(book);
 }
